193b588a1da6847354f831cd6acdfd7dc1475d86
unknown <arthurdamiron@gmail.com>
2021-01-20 15:36:16 +0100

    runbuild

---
+++ pages/[stream].js
@@ -0,0 +1,28 @@
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import Navbar from "@components/Navbar";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter !</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </Head>
+
+      <Navbar />
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
